1c0564ea01d15f87ce3bc749ff7a6ba44033edde
unknown <charliegarvin90@gmail.com>
2023-04-29 16:02:00 -0600

    added functionality to add and delete notes

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const apiRoute = require('./routes/apiRoute');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('./public'));
+app.use('/api', apiRoute);
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
